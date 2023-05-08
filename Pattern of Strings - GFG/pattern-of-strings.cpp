3fd80096ece8f57e16849ccd35ff9d3fa2c635b3
Vinaykumaryadav9199 <132898839+Vinaykumaryadav9199@users.noreply.github.com>
2023-05-08 23:16:44 +0530

    Added solution - LeetHub

---
+++ Pattern of Strings - GFG/pattern-of-strings.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	vector<string> pattern(string S)
+	{
+	    // Your code goes here
+	    int x = S.length()-1;
+	    vector <string> ans;
+	    for (int i=0;i<S.length();i++)
+	    {   string temp ="";
+	        for (int j=0;j<=x;j++)
+	        {
+	            temp+=S[j];
+	        }
+	        
+	        x--;
+	        ans.push_back(temp);
+	    }
+	    
+	    return ans ;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	string tc;
+   	getline(cin, tc);
+   	t = stoi(tc);
+   	while(t--)
+   	{
+   		string s;
+   		getline(cin, s);
+
+   		Solution ob;
+
+   		vector<string> ans = ob.pattern(s);
+
+   		for(int i = 0; i < ans.size(); i++)
+   			cout << ans[i] << "\n";
+   		
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
